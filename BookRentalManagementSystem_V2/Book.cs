4859670d767817b76f214cbbc8114ebcf5d03902
Sajilan P. <UT01261@unicomtic.com>
2024-10-11 16:41:44 +0530

    question ten

---
+++ BookRentalManagementSystem_V2/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalManagementSystem_V2
+{
+    internal class Book
+    {
+        public Book() { }
+        public string bookId { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public decimal rentalPrice { get; set; }
+
+
+        public Book(string Bookid, string Title, string Author, decimal RentalPrice)
+        {
+            bookId = Bookid;
+            title = Title;
+            author = Author;
+            rentalPrice = RentalPrice;
+           
+        }
+    }
+}
